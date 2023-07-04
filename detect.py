c6ac80fc3a2276b50550b768c935eb20fad92c30
abhishek agrawal <abhiagwl4262@gmail.com>
2023-07-05 02:45:54 +0530

    blacked

--- detect.py
+++ detect.py
@@ -1,6 +1,9 @@
 import os
+import cv2
 import sys
 import argparse
+import numpy as np
+import pandas as pd
 from pathlib import Path
 
 import torch
@@ -13,30 +16,42 @@ ROOT = Path(os.path.relpath(ROOT, Path.cwd()))  # relative
 
 from models.common import DetectMultiBackend
 from utils.dataloaders import LoadImages
-from utils.general import (LOGGER, Profile, check_img_size, check_requirements, colorstr,
-                           increment_path, non_max_suppression, print_args, scale_boxes, xyxy2xywh)
+from utils.general import (
+    LOGGER,
+    Profile,
+    check_img_size,
+    check_requirements,
+    non_max_suppression,
+    print_args,
+    scale_boxes,
+    xyxy2xywh,
+    increment_path,
+)
 from utils.torch_utils import select_device, smart_inference_mode
 
 
 @smart_inference_mode()
 def run(
-        output= ROOT / 'output',
-        weights=ROOT / 'yolov5s.pt',  # model path or triton URL
-        source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
-        data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
-        imgsz=(640, 640),  # inference size (height, width)
-        conf_thres=0.4,  # confidence threshold
-        iou_thres=0.4,  # NMS IOU threshold
-        max_det=1000,  # maximum detections per image
-        device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
-        save_txt=False,  # save results to *.txt
-        save_conf=False,  # save confidences in --save-txt labels
+    output=ROOT / "output",
+    weights=ROOT / "yolov5s.pt",  # model path or triton URL
+    source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
+    data=ROOT / "data/coco128.yaml",  # dataset.yaml path
+    imgsz=(640, 640),  # inference size (height, width)
+    conf_thres=0.4,  # confidence threshold
+    iou_thres=0.4,  # NMS IOU threshold
+    max_det=1000,  # maximum detections per image
+    device="",  # cuda device, i.e. 0 or 0,1,2,3 or cpu
+    save_txt=False,  # save results to *.txt
+    save_conf=False,  # save confidences in --save-txt labels
+    visualize=True # save images with bbs
 ):
     source = str(source)
 
-    # Directories
+    # # Directories
     save_dir = increment_path(Path(output), exist_ok=True)  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
+    (save_dir / "labels" if save_txt else save_dir).mkdir(
+        parents=True, exist_ok=True
+    )  # make dir
 
     # Load model
     device = select_device(device)
@@ -45,12 +60,20 @@ def run(
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
     # Dataloader
-    bs = 1  # batch_size
     dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt, vid_stride=1)
 
     # Run inference
-    model.warmup(imgsz=(1 if pt or model.triton else bs, 3, *imgsz))  # warmup
-    seen, windows, dt = 0, [], (Profile(), Profile(), Profile())
+    model.warmup(imgsz=(1, 3, *imgsz))  # warmup
+    seen, _, dt = 0, [], (Profile(), Profile(), Profile())
+    labels_dict = {
+        "filename": [],
+        "xmin": [],
+        "ymin": [],
+        "xmax": [],
+        "ymax": [],
+        "class": [],
+    }
+
     for path, im, im0s, _, s in dataset:
         with dt[0]:
             im = torch.from_numpy(im).to(model.device)
@@ -65,17 +88,17 @@ def run(
 
         # NMS
         with dt[2]:
-            pred = non_max_suppression(pred, conf_thres, iou_thres, None, False, max_det=max_det)
+            pred = non_max_suppression(
+                pred, conf_thres, iou_thres, None, False, max_det=max_det
+            )
 
         # Process predictions
         for i, det in enumerate(pred):  # per image
             seen += 1
-            p, im0, frame = path, im0s.copy(), getattr(dataset, 'frame', 0)
-
+            p, im0 = path, im0s.copy()
+            h, w = im0s.shape[:2]
             p = Path(p)  # to Path
-            save_path = str(save_dir / p.name)  # im.jpg
-            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # im.txt
-            s += '%gx%g ' % im.shape[2:]  # print string
+            s += "%gx%g " % im.shape[2:]  # print string
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
                 # Rescale boxes from img_size to im0 size
@@ -87,37 +110,120 @@ def run(
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
 
                 # Write results
+                image_name = os.path.basename(path)
                 for *xyxy, conf, cls in reversed(det):
                     if save_txt:  # Write to file
-                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
-                        line = (cls, *xywh, conf) if save_conf else (cls, *xywh)  # label format
-                        with open(f'{txt_path}.txt', 'a') as f:
-                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                        xywh = (
+                            (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn)
+                            .view(-1)
+                            .tolist()
+                        )  # normalized xywh
+                        line = (
+                            (cls, *xywh, conf) if save_conf else (cls, *xywh)
+                        )  # label format
+                        label = int(line[0].detach().cpu().numpy().tolist())
+
+                        xc, yc, w_box, h_box = line[1:]
+                        xc = float(xc) * w
+                        yc = float(yc) * h
+                        w_box = float(w_box) * w
+                        h_box = float(h_box) * h
+
+                        x1 = int(xc - w_box / 2.0)
+                        y1 = int(yc - h_box / 2.0)
+                        x2 = int(xc + w_box / 2.0)
+                        y2 = int(yc + h_box / 2.0)
+
+                        labels_dict["filename"].append(image_name)
+                        labels_dict["xmin"].append(x1)
+                        labels_dict["ymin"].append(y1)
+                        labels_dict["xmax"].append(x2)
+                        labels_dict["ymax"].append(y2)
+                        labels_dict["class"].append(label)
+
+                        if visualize:
+                            cv2.rectangle(im0s, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            else:
+                labels_dict["filename"].append(image_name)
+                labels_dict["xmin"].append(0)
+                labels_dict["ymin"].append(0)
+                labels_dict["xmax"].append(1)
+                labels_dict["ymax"].append(1)
+                labels_dict["class"].append(0)
+
+        if visualize:
+            out_path = os.path.join(str(save_dir), image_name)
+            cv2.imwrite(out_path, im0s)
 
         # Print time (inference-only)
-        LOGGER.info(f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms")
+        LOGGER.info(
+            f"{s}{'' if len(det) else '(no detections), '}{dt[1].dt * 1E3:.1f}ms"
+        )
 
     # Print results
-    t = tuple(x.t / seen * 1E3 for x in dt)  # speeds per image
-    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}' % t)
-    if save_txt:
-        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
-        LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
+    t = tuple(x.t / seen * 1e3 for x in dt)  # speeds per image
+    LOGGER.info(
+        f"Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS per image at shape {(1, 3, *imgsz)}"
+        % t
+    )
+
+    df = pd.DataFrame(labels_dict, index=None)
+    np.savetxt(
+        "results.csv", df, delimiter=";", fmt=["%s", "%d", "%d", "%d", "%d", "%d"]
+    )
 
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--output', type=str, default=ROOT / 'output', help='path to save model output')
-    parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'yolov5s.pt', help='model path or triton URL')
-    parser.add_argument('--source', type=str, default=ROOT / 'data/images', help='file/dir/URL/glob/screen/0(webcam)')
-    parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
-    parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
-    parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
-    parser.add_argument('--iou-thres', type=float, default=0.45, help='NMS IoU threshold')
-    parser.add_argument('--max-det', type=int, default=1000, help='maximum detections per image')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
+    parser.add_argument(
+        "--output", type=str, default=ROOT / "output", help="path to save model output"
+    )
+    parser.add_argument(
+        "--weights",
+        nargs="+",
+        type=str,
+        default=ROOT / "yolov5s.pt",
+        help="model path or triton URL",
+    )
+    parser.add_argument(
+        "--source",
+        type=str,
+        default=ROOT / "data/images",
+        help="file/dir/URL/glob/screen/0(webcam)",
+    )
+    parser.add_argument(
+        "--data",
+        type=str,
+        default=ROOT / "data/coco128.yaml",
+        help="(optional) dataset.yaml path",
+    )
+    parser.add_argument(
+        "--imgsz",
+        "--img",
+        "--img-size",
+        nargs="+",
+        type=int,
+        default=[640],
+        help="inference size h,w",
+    )
+    parser.add_argument(
+        "--conf-thres", type=float, default=0.25, help="confidence threshold"
+    )
+    parser.add_argument(
+        "--iou-thres", type=float, default=0.45, help="NMS IoU threshold"
+    )
+    parser.add_argument(
+        "--max-det", type=int, default=1000, help="maximum detections per image"
+    )
+    parser.add_argument(
+        "--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu"
+    )
+    parser.add_argument("--save-txt", action="store_true", help="save results to *.txt")
+    parser.add_argument(
+        "--save-conf", action="store_true", help="save confidences in --save-txt labels"
+    )
+    parser.add_argument('--visualize', action='store_true',
+                        help='visualize results')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
@@ -125,10 +231,10 @@ def parse_opt():
 
 
 def main(opt):
-    check_requirements(ROOT / 'requirements.txt', exclude=('tensorboard', 'thop'))
+    check_requirements(ROOT / "requirements.txt", exclude=("tensorboard", "thop"))
     run(**vars(opt))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     opt = parse_opt()
     main(opt)
